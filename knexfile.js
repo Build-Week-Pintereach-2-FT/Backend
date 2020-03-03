ad2e44bbe1dc7446e1209fbf755f842fc532e5c7
Timothy <timgill987@gmail.com>
2020-03-02 21:41:51 -0500

    working with deployment.......

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,16 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/pintereach.db3'
+      filename: "./data/pintereach.db3"
     },
     pool: {
       afterCreate: (conn, done) => {
         // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+      }
     }
   },
 
@@ -32,19 +32,18 @@ module.exports = {
   // },
 
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
- 
+
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-     directory: "./migrations"
+      directory: "./migrations"
     },
     seeds: {
       directory: "./seeds"
     }
   }
-
-};
+}
