55cbb02a7ff4f21153f9d6fb3a806a5527893249
Timothy <timgill987@gmail.com>
2020-03-02 21:30:45 -0500

    working with deployment.......

--- seeds/003-Articles.js
+++ seeds/003-Articles.js
@@ -10,42 +10,42 @@ exports.seed = function(knex) {
           articleName: "Dreams revolution",
           linkToArticle: "https://www.gamerevolution.com/game/dreams#/slide/1",
           categories: "Video Games",
-          datePublished: "2020-27-2",
+          datePublished: "",
           userId: 1
         },
         {
           articleName: "Mass Effect Andromeda sucks",
           linkToArticle: "https://medium.com/@jaredleejohnson/bioware-cant-patch-what-sucks-about-mass-effect-andromeda-95192dc9bea4",
           categories: "Video Games",
-          datePublished: "2020-03-02",
+          datePublished: "",
           userId: 1
         },
         {
           articleName: "Ketogenic egg sammies",
           linkToArticle: "https://www.carbmanager.com/recipe/keto-hard-boiled-egg-breakfast-bites",
           categories: "Cooking",
-          datePublished: "2020-03-02",
+          datePublished:"",
           userId: 3
         },
         {
           articleName: "haunted house of ",
           linkToArticle: "https://georgiastatesignal.com/six-atlantas-scariest-haunted-houses/",
           categories: "Attractions",
-          datePublished: "2020-03-02",
+          datePublished: "",
           userId: 2
         },
         {
           articleName: "Gatorland in flames",
           linkToArticle: "https://www.nytimes.com/2006/11/07/us/07gator.html",
           categories: "News",
-          datePublished: "2006-11-07",
+          datePublished: "",
           userId: 4
         },
         {
           articleName: "kidnaping definition ",
           linkToArticle: "https://www.dictionary.com/browse/kidnaping",
           categories: "Misc",
-          datePublished: "2020-03-02",
+          datePublished: "",
           userId: 5
         },
       ]);
